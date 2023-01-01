245d6d44ef2ac97f11fbf9abfe3dd1278b678b09
Aashish Patil <aashish@appsdeployer.com>
2023-01-01 22:39:58 +0530

    first

---
+++ html 5 day/script.js
@@ -0,0 +1,16 @@
+// pop_login
+
+  function openForm() {
+    document.getElementById("forn_id_pop").style.display = "block";
+    }
+    function closeForm() {
+    document.getElementById("forn_id_pop").style.display = "none";
+    }
+    function opForm() {
+    document.getElementById("sign_id_pop").style.display = "block";
+    }
+    function form_close() {
+    document.getElementById("sign_id_pop").style.display = "none";
+    }
+
+
---
+++ photo html/script.js
@@ -0,0 +1,12 @@
+function openForm() {
+    document.getElementById("forn_id_pop").style.display = "block";
+    }
+    function closeForm() {
+    document.getElementById("forn_id_pop").style.display = "none";
+    }
+    function opForm() {
+    document.getElementById("sign_id_pop").style.display = "block";
+    }
+    function form_close() {
+    document.getElementById("sign_id_pop").style.display = "none";
+    }
